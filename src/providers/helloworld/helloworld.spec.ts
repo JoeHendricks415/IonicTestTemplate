5681df6c0b922ce9b176d8b31e74bfab4ac8b079
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-04-03 13:18:58 -0400

    Ionic Template Test

---
+++ src/providers/helloworld/helloworld.spec.ts
@@ -0,0 +1,11 @@
+import { HelloworldProvider } from './helloworld';
+ 
+describe('Magic 8 Ball Service', () => {
+ 
+    it('should do nothing', () => {
+ 
+        expect(true).toBeTruthy();
+ 
+    });
+ 
+});
